837fa1f79b8e0f99ecc3c1d0c7f1911d6c9016ed
Sacha Malécot <sacha.malecot@gmail.com>
2023-05-03 06:18:24 +0200

    change logo color

--- src/components/DarkMode/index.jsx
+++ src/components/DarkMode/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import moon from '../../assets/images/moon.svg';
 import styles from "./styles.module.scss";
 
 const DarkMode = () => {
@@ -12,9 +13,7 @@ const DarkMode = () => {
  
   return (
     <div>
-      <button onClick={changeTheme} className={styles.__button}>
-        <svg className={theme ? styles.__fill : styles.__empty} viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"><rect fill="none" height="256" width="256"/><path d="M216.7,152.6A91.9,91.9,0,0,1,103.4,39.3h0A92,92,0,1,0,216.7,152.6Z" fill="currentColor" stroke="$strokeColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/></svg>
-      </button>
+      <button onClick={changeTheme} className={styles.__button}><img className='logo' src={moon} alt="moon logo" /></button>
     </div>
   );
 }
