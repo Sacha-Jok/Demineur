d76156cd980e0362662f8a74d6e89698a841f7c6
Sacha Malécot <sacha.malecot@gmail.com>
2023-05-05 17:25:11 +0200

    add tuto steps

--- src/pages/Tuto/index.jsx
+++ src/pages/Tuto/index.jsx
@@ -1,9 +1,13 @@
 import Navbar from '../../components/Navbar';
+import Step from '../../components/Step';
+
+import styles from "./styles.module.scss";
 
 const Tuto = () => {
     return (
         <div>
             <Navbar />
+            <div className={styles.__steps}><Step /></div>
         </div>
     );
 };
