f90020824473cd4b32e67c1daaedc1a52bef9604
Sacha Malécot <sacha.malecot@gmail.com>
2023-05-08 16:31:03 +0200

    add Game comp

--- src/pages/Play/index.jsx
+++ src/pages/Play/index.jsx
@@ -1,9 +1,13 @@
 import Navbar from '../../components/Navbar';
+import Game from '../../components/Game';
+
+import styles from "./styles.module.scss";
 
 const Play = () => {
     return (
         <div>
             <Navbar />
+            <div id='gameSection'><Game /></div>
         </div>
     );
 };
