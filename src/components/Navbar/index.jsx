e536d5a4d24e7d91a3a9e76ea24059ac1876c8fe
Sacha Malécot <sacha.malecot@gmail.com>
2023-05-08 13:12:34 +0200

    change path

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -8,12 +8,12 @@ const Navbar = () => (
     <nav className={styles.__navbar}>
       <div className={styles.__container}>
         <Link to="/tuto" className={styles.__link}><h2 className={styles.__subtitle}>Comment jouer ?</h2></Link>
-        <a href="/play">
+        <Link to="/">
           <div className={styles.__logo}>
             <img className={`logo ${styles.__bomb}`} src={logo} alt="bomb logo" />
             <h1 className={styles.__title}>DEMINEUR</h1>
           </div>
-        </a>
+        </Link>
         <Link to="/leaderboard" className={styles.__link}><h2 className={styles.__subtitle}>Classement</h2></Link>
       </div>
       <div className={styles.__darkLogo}>
