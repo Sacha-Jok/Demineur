1af3ac31d692471b536fda022aa048baeeaa34b7
Sacha Malécot <sacha.malecot@gmail.com>
2023-05-09 15:19:40 +0200

    css correction

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,20 +6,11 @@ import DarkMode from '../DarkMode';
 
 const Navbar = () => (
     <nav className={styles.__navbar}>
-      <div className={styles.__container}>
-        <Link to="/tuto" className={styles.__link}><h2 className={styles.__subtitle}>Comment jouer ?</h2></Link>
-        <Link to="/">
-          <div className={styles.__logo}>
-            <img className={`logo ${styles.__bomb}`} src={logo} alt="bomb logo" />
-            <h1 className={styles.__title}>DEMINEUR</h1>
-          </div>
-        </Link>
-        <Link to="/leaderboard" className={styles.__link}><h2 className={styles.__subtitle}>Classement</h2></Link>
-      </div>
-      <div className={styles.__darkLogo}>
-        <DarkMode />
-      </div>
-      
+      <div></div>
+      <Link to="/tuto" className={styles.__link}><h2 className={styles.__subtitle}>Comment jouer ?</h2></Link>
+      <Link to="/"><div className={styles.__logo}><img className={`logo ${styles.__bomb}`} src={logo} alt="bomb logo" /><h1 className={styles.__title}>DEMINEUR</h1></div></Link>
+      <Link to="/leaderboard" className={styles.__link}><h2 className={styles.__subtitle}>Classement</h2></Link>
+      <DarkMode className={styles.__darkLogo} />
     </nav>
 );
 
