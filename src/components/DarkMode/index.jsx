71762cff332dbb2d34b7f6a9208677e037593e2f
Sacha Malécot <sacha.malecot@gmail.com>
2023-05-26 12:52:55 +0200

    darkmode save to local storage

--- src/components/DarkMode/index.jsx
+++ src/components/DarkMode/index.jsx
@@ -1,22 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import moon from '../../assets/images/moon.svg';
-import sun from '../../assets/images/sun.svg';
 import styles from "./styles.module.scss";
 
 const DarkMode = () => {
-  const [theme, setTheme] = useState(false);
-
+  const [theme, setTheme] = useState('light');
+  
   const changeTheme = () => {
-    setTheme(!theme)
-    document.body.classList.toggle("dark");
-  }
+    if (theme === 'dark') {
+      localStorage.setItem('theme', 'light');
+      setTheme('light');
+    } else {
+      localStorage.setItem('theme', 'dark');
+      setTheme('dark');
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem('theme');
+    if (localTheme) {
+      setTheme(localTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [theme]);
  
   return (
+  <>
     <div>
       <button onClick={changeTheme} className={styles.__button}><img className="logo moon" src={moon} alt="moon logo" /></button>
     </div>
+  </>
   );
+  
 }
 
 export default DarkMode;
