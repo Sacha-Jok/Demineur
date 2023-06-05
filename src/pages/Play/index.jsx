ac6643b4760cda3fbccaee0b0725c8ca5d20539c
Sacha Malécot <sacha.malecot@gmail.com>
2023-06-05 13:19:13 +0200

    archi correction

--- src/pages/Play/index.jsx
+++ src/pages/Play/index.jsx
@@ -1,15 +1,12 @@
 import Navbar from '../../components/Navbar';
 import Game from '../../components/Game';
 
-import styles from "./styles.module.scss";
-
-
 const Play = () => {
     return (
-        <div>
-            <Navbar />
-            <div className={styles.__gameSection}><Game /></div>
-        </div>
+    <>
+        <Navbar />
+        <Game />
+    </>       
     );
 };
 
